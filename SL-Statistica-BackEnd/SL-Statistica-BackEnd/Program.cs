7d9a579d7ec4ac5e3b6aec9a33b54e29c3c98c97
Simeï Steiner <simei.steiner@gmail.com>
2023-10-18 12:43:57 +0200

    feature: Add LotterApi endpoint 'latestDraw' and a query endpoint

---
+++ SL-Statistica-BackEnd/SL-Statistica-BackEnd/Program.cs
@@ -0,0 +1,22 @@
+using DataManager;
+using SL_Statistica_BackEnd.Apis;
+using Utilities.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LotteryDbContext>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+app.AddGeneralLotteryApi();
+app.Run();
