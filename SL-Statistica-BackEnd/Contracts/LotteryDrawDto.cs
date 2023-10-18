2c470b1360b03fa819a40d43ddb0c3314d6b6827
Simeï Steiner <simei.steiner@gmail.com>
2023-10-18 12:40:38 +0200

    feature: Add Contracts (LotterDrawDto and LotteryDrawQueryParameters)

---
+++ SL-Statistica-BackEnd/Contracts/LotteryDrawDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Contracts;
+public record LotteryDrawDto
+{
+    public required int NumberOne { get; init; }
+    public required int NumberTwo { get; init; }
+    public required int NumberThree { get; init; }
+    public required int NumberFour { get; init; }
+    public required int NumberFive { get; init; }
+    public required int NumberSix { get; init; }
+    public required int LuckyNumber { get; init; }
+    public required DateTime Date { get; init; }
+    public int? JackPot { get; init; }
+}
---
+++ SL-Statistica-BackEnd/Contracts/LotteryDrawQueryParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace Contracts;
+public class LotteryDrawQueryParameters
+{
+    public DateTime? Date { get; set; }
+    public int? Take { get; set; }
+    public int? Skip { get; set; }
+    public DateTime? FromDate { get; set; }
+    public DateTime? ToDate { get; set; }
+    public bool OrderByAscending { get; set; }
+}
+
+public class Numbers
+{
+    public int One { get; set; }
+    public int Two { get; set; }
+}
