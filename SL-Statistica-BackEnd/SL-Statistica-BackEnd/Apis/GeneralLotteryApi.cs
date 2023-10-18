f5bdc8f31fc5eeb8be11d9039c3bf91703c73157
Simeï Steiner <simei.steiner@gmail.com>
2023-10-18 12:43:57 +0200

    feature: Add LotterApi endpoint 'latestDraw' and a query endpoint

---
+++ SL-Statistica-BackEnd/SL-Statistica-BackEnd/Apis/GeneralLotteryApi.cs
@@ -0,0 +1,35 @@
+﻿using Contracts;
+using DataManager;
+using DataManager.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Utilities.Services;
+using Utilities.Verification;
+
+namespace SL_Statistica_BackEnd.Apis;
+
+public static class GeneralLotteryApi
+{
+    public static WebApplication AddGeneralLotteryApi(this WebApplication app)
+    {
+        app.MapGet("/LatestDraw", async Task<Results<Ok<LotteryDrawDto>, BadRequest>> (LotteryDbContext ctx) =>
+        {
+            var lastDraw = await ctx.Draws.OrderBy(l => l.Date).LastOrDefaultAsync();
+            if (lastDraw != null)
+            {
+                return TypedResults.Ok(lastDraw.MapToDto());
+            }
+            return TypedResults.BadRequest();
+        });
+
+        app.MapGet("/",  Results<Ok<IEnumerable<LotteryDrawDto>>, BadRequest> (LotteryDbContext ctx, [AsParameters] LotteryDrawQueryParameters parameters) =>
+        {
+            var draws = ctx.Draws.FilterByParameter(parameters);
+            return TypedResults.Ok(draws.Select(d => d.MapToDto()));
+        });
+        return app;
+    }
+
+}
+
+
