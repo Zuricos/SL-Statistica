e5664605a025ee515a8c82c144f6e6dc878d86b2
Simeï Steiner <simei.steiner@gmail.com>
2023-10-26 20:47:42 +0200

    feat: Add year param to UpdateDb

---
+++ SL-Statistica-BackEnd/Utilities/Verification/IntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Utilities.Verification;
+public static class IntExtension
+{
+	/// <summary>
+	/// Returns true when year lies between 2013 and the current year
+	/// </summary>
+	/// <param name="year"></param>
+	public static bool IsYearInLotteryDrawTimeSpan(this int year)
+	{
+		if (year > DateTime.Now.Year) return false;
+		if (year < 2013) return false;
+		return true;
+	}
+}
+
