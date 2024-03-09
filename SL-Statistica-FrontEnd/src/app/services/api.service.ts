9d95495cf3da79bec6e58145e64354332d372ec5
Zuricos <simei.steiner@gmail.com>
2024-03-09 11:00:41 +0100

    wip: api client

--- SL-Statistica-FrontEnd/src/app/services/api.service.ts
+++ SL-Statistica-FrontEnd/src/app/services/api.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private baseUrl = 'http://localhost:5000'; 
 
   constructor(private http: HttpClient) { }
 
-  getData() {
-    return this.http.get('https://localhost:5000');
+  getLatestDraw(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/LatestDraw`);
   }
+
+  getDraws(parameters: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/`, { params: parameters });
+  }
+
+  updateDB(year: number, webScrapper: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/UpdateDB`, { year, webScrapper });
+  }
+}
+
+export interface LotteryDrawDto {
+  numberOne: number;
+  numberTwo: number;
+  numberThree: number;
+  numberFour: number;
+  numberFive: number;
+  numberSix: number;
+  luckyNumber: number;
+  date: Date;
+  jackPot?: number;
 }
